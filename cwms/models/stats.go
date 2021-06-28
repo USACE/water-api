b1997b0c90c63ed2235a509f42274f230bbe1e62
Brett Palmberg <29417036+brettpalmberg@users.noreply.github.com>
2021-06-28 12:52:36 -0500

    code review suggestions (#9)

--- cwms/models/stats.go
+++ cwms/models/stats.go
@@ -8,53 +8,59 @@ import (
 	"github.com/jackc/pgx/v4/pgxpool"
 )
 
-var ListStatsStatesBaseSQL = `WITH
-	state_location_counts AS (
-		SELECT l.state_id, COUNT(*)
-		FROM a2w_cwms.location l
-		WHERE l.state_id IS NOT null
-		GROUP BY l.state_id
-	),
-		state_project_counts AS (
-		SELECT l.state_id, COUNT(*)
-		FROM a2w_cwms.location l
-		WHERE l.state_id IS NOT null AND l.kind_id = (SELECT id FROM a2w_cwms.location_kind lk WHERE lk.name = 'PROJECT')
-		GROUP BY l.state_id
-	)
-	SELECT l.state_id,
-		l.count AS locations,
-		p.count AS projects
-	FROM state_location_counts l
-	JOIN state_project_counts p ON p.state_id = l.state_id`
+var ListStatsStatesBaseSQL = `WITH state_location_counts AS (
+						          SELECT state_id, COUNT(*)
+	                         	  FROM location
+	                         	  WHERE state_id IS NOT null
+	                         	  GROUP BY state_id
+	                          ),
+							  state_project_counts AS (
+								  SELECT state_id, COUNT(*)
+								  FROM location
+								  WHERE state_id IS NOT null
+								  	AND kind_id = (SELECT id FROM location_kind WHERE UPPER(name) = 'PROJECT')
+								  GROUP BY state_id
+	                         )
+	                         SELECT l.state_id,
+							        l.count AS locations,
+									p.count AS projects
+	                         FROM state_location_counts l
+	                         JOIN state_project_counts p ON p.state_id = l.state_id`
 
-var ListStatsOfficeBaseSQL = `WITH
-	office_locations AS (
-		SELECT l.office_id, count(*) AS locations
-		FROM a2w_cwms.location l
-		GROUP BY l.office_id
-	)
-	SELECT office_locations.office_id, office_locations.locations
-	FROM office_locations`
+var ListStatsOfficeBaseSQL = `WITH office_location_counts AS (
+	                              SELECT office_id, COUNT(*)
+								  FROM location
+								  WHERE office_id IS NOT null
+								  GROUP BY office_id
+							 ),
+							 office_project_counts AS (
+								SELECT office_id, COUNT(*)
+								FROM location
+								WHERE state_id IS NOT null
+								  AND kind_id = (SELECT id FROM location_kind WHERE UPPER(name) = 'PROJECT')
+								GROUP BY office_id
+							 )
+							 SELECT l.office_id,
+							        l.count AS locations,
+									p.count AS projects
+							 FROM office_location_counts l
+							 JOIN office_project_counts p ON p.office_id = l.office_id`
 
 type StatsState struct {
-	StateID   int8  `json:"state_id"`
-	Locations int16 `json:"locations"`
-	Projects  int16 `json:"projects"`
+	StateID   int `json:"state_id"`
+	Locations int `json:"locations"`
+	Projects  int `json:"projects"`
 }
 
 type StatsOffice struct {
 	OfficeID  uuid.UUID `json:"office_id"`
-	Locations int16     `json:"locations"`
+	Locations int       `json:"locations"`
+	Projects  int       `json:"projects"`
 }
 
 func ListStatsStates(db *pgxpool.Pool) ([]StatsState, error) {
 	ss := make([]StatsState, 0)
-	if err := pgxscan.Select(
-		context.Background(),
-		db,
-		&ss,
-		ListStatsStatesBaseSQL,
-	); err != nil {
+	if err := pgxscan.Select(context.Background(), db, &ss, ListStatsStatesBaseSQL); err != nil {
 		return make([]StatsState, 0), err
 	}
 	return ss, nil
@@ -63,11 +69,8 @@ func ListStatsStates(db *pgxpool.Pool) ([]StatsState, error) {
 func GetStatsState(db *pgxpool.Pool, id *string) (*StatsState, error) {
 	var ss StatsState
 	if err := pgxscan.Get(
-		context.Background(),
-		db,
-		&ss,
-		ListStatsStatesBaseSQL+" WHERE l.state_id = $1",
-		id,
+		context.Background(), db, &ss,
+		ListStatsStatesBaseSQL+" WHERE l.state_id = $1", id,
 	); err != nil {
 		return nil, err
 	}
@@ -76,12 +79,7 @@ func GetStatsState(db *pgxpool.Pool, id *string) (*StatsState, error) {
 
 func ListStatsOffices(db *pgxpool.Pool) ([]StatsOffice, error) {
 	so := make([]StatsOffice, 0)
-	if err := pgxscan.Select(
-		context.Background(),
-		db,
-		&so,
-		ListStatsOfficeBaseSQL,
-	); err != nil {
+	if err := pgxscan.Select(context.Background(), db, &so, ListStatsOfficeBaseSQL); err != nil {
 		return make([]StatsOffice, 0), err
 	}
 	return so, nil
@@ -90,11 +88,8 @@ func ListStatsOffices(db *pgxpool.Pool) ([]StatsOffice, error) {
 func GetStatsOffice(db *pgxpool.Pool, officeID *uuid.UUID) (*StatsOffice, error) {
 	var so StatsOffice
 	if err := pgxscan.Get(
-		context.Background(),
-		db,
-		&so,
-		ListStatsOfficeBaseSQL+" WHERE office_locations.office_id = $1",
-		officeID,
+		context.Background(), db, &so,
+		ListStatsOfficeBaseSQL+" WHERE l.office_id = $1", officeID,
 	); err != nil {
 		return nil, err
 	}
