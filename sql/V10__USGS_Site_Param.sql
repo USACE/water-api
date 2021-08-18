c6080b37eabf23864ed27d0c7a36845552e68827
Adam Scarberry <46494851+adamscarberry@users.noreply.github.com>
2021-08-18 09:02:19 -0400

    update v_usgs_site_parameters_enabled, roles grants, fix seed data

--- sql/V10__USGS_Site_Param.sql
+++ sql/V10__USGS_Site_Param.sql
@@ -16,20 +16,39 @@ CREATE TABLE IF NOT EXISTS usgs_site_parameters (
 -- Create v_usgs_site_parameters_enabled
 CREATE OR REPLACE VIEW v_usgs_site_parameters_enabled AS (
     SELECT
-    usp.usgs_site_id,
+    distinct usp.usgs_site_id,
     us.usgs_id,
-    code_agg.param_codes as usgs_parameter
+    code_agg.usgs_parameter_code,
+    param_id_agg.usgs_parameter_id
     FROM a2w_cwms.usgs_site_parameters usp
-    --join a2w_cwms.usgs_parameter up on up.id = usp.usgs_parameter_id 
     JOIN a2w_cwms.usgs_site us ON us.id = usp.usgs_site_id
     INNER JOIN (
-        SELECT string_agg(code::text, ',') AS param_codes, b.usgs_site_id 
+        SELECT array_agg(usgs_parameter_id) AS usgs_parameter_id, a.usgs_site_id 
+        FROM a2w_cwms.usgs_site_parameters a
+        JOIN a2w_cwms.usgs_parameter b ON a.usgs_parameter_id = b.id
+        GROUP BY a.usgs_site_id 
+        ) param_id_agg ON param_id_agg.usgs_site_id = us.id	
+    INNER JOIN (
+        SELECT array_agg(code) AS usgs_parameter_code, b.usgs_site_id 
         FROM a2w_cwms.usgs_parameter a
         JOIN a2w_cwms.usgs_site_parameters b ON b.usgs_parameter_id = a.id
         GROUP BY b.usgs_site_id 
         ) code_agg ON code_agg.usgs_site_id = us.id	
-    ORDER BY us.name
+    ORDER BY us.usgs_id
 );
+
+-- Grant read
+GRANT SELECT ON
+    usgs_parameter,
+    usgs_site_parameters,
+    v_usgs_site_parameters_enabled
+TO water_reader;
+
+-- Grant write
+GRANT INSERT,UPDATE,DELETE ON
+    usgs_parameter,
+    usgs_site_parameters
+TO water_writer;
 
 -- usgs_parameter seed data
 INSERT INTO usgs_parameter (id, code, description) VALUES 
@@ -44,8 +63,8 @@ INSERT INTO usgs_parameter (id, code, description) VALUES
 -- usgs_site_parameters seed data for testing
 INSERT INTO usgs_site_parameters (id, usgs_site_id, usgs_parameter_id) VALUES
 -- GUYANDOTTE RIVER AT LOGAN, WV - Stage and Flow
-('2a8c983a-2210-490b-a18d-55533a048f4a', '904f6183-899b-4c35-ae5a-13292475a005', 'a9f78261-e6a6-4ad2-827e-bd7a4ac0dc28'),
-('b5ad3c36-4238-4fbb-8b0d-a5d544479eac', '904f6183-899b-4c35-ae5a-13292475a005', 'ba29fc34-6315-4424-838f-9b1863715fad'),
+('2a8c983a-2210-490b-a18d-55533a048f4a', (select id from usgs_site where name='GUYANDOTTE RIVER AT LOGAN, WV'), 'a9f78261-e6a6-4ad2-827e-bd7a4ac0dc28'),
+('b5ad3c36-4238-4fbb-8b0d-a5d544479eac', (select id from usgs_site where name='GUYANDOTTE RIVER AT LOGAN, WV'), 'ba29fc34-6315-4424-838f-9b1863715fad'),
 -- GUYANDOTTE RIVER AT BRANCHLAND, WV - Stage and Precip
-('1fdd9651-84ab-4d17-9e6f-37a5c2596521', '46a85fa5-c7e6-4d80-831f-faa8a96206ba', 'a9f78261-e6a6-4ad2-827e-bd7a4ac0dc28'),
-('ef9538de-8e44-4827-b552-0498ef1c18ff', '46a85fa5-c7e6-4d80-831f-faa8a96206ba', '738eb4df-b34b-45cc-a5aa-f2136384244f');
+('1fdd9651-84ab-4d17-9e6f-37a5c2596521', (select id from usgs_site where name='GUYANDOTTE RIVER AT BRANCHLAND, WV'), 'a9f78261-e6a6-4ad2-827e-bd7a4ac0dc28'),
+('ef9538de-8e44-4827-b552-0498ef1c18ff', (select id from usgs_site where name='GUYANDOTTE RIVER AT BRANCHLAND, WV'), '738eb4df-b34b-45cc-a5aa-f2136384244f');
