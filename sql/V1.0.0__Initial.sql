a77c93f8b0f4a5295c78770be33d503d85638d73
Brett Palmberg <brett.t.palmberg@usace.army.mil>
2021-10-26 14:41:44 -0500

    trailing comma

--- sql/V1.0.0__Initial.sql
+++ sql/V1.0.0__Initial.sql
@@ -323,7 +323,7 @@ INSERT INTO watershed (id,slug,"name",geometry,office_id) VALUES
     ('ad30f178-afc3-43b9-ba92-7bd139581217','red-river-north','Red River North',ST_GeomFromText('POLYGON ((-356000 2950000, 150000 2950000, 150000 2494000, -356000 2494000, -356000 2950000))',5070),'2cf60156-f22a-418a-bc9f-a28960ad0321'),
     ('c8bf6c6d-7f19-406a-a438-f2f876ce4815','souris-river','Souris River',ST_GeomFromText('POLYGON ((-708000 3100000, -178000 3100000, -178000 2736000, -708000 2736000, -708000 3100000))',5070),'2cf60156-f22a-418a-bc9f-a28960ad0321'),
     ('ced6ec9e-43b5-496e-a2b7-894af92c9b63','mississippi-river-navigation','Mississippi River Navigation',ST_GeomFromText('POLYGON ((48000 2646000, 564000 2646000, 564000 2204000, 48000 2204000, 48000 2646000))',5070),'2cf60156-f22a-418a-bc9f-a28960ad0321'),
-    ('f4219691-e498-46a3-ab0f-f2957bd09a10','minnesota-river','Minnesota River',ST_GeomFromText('POLYGON ((-112000 2602000, 234000 2602000, 234000 2244000, -112000 2244000, -112000 2602000))',5070),'2cf60156-f22a-418a-bc9f-a28960ad0321')
+    ('f4219691-e498-46a3-ab0f-f2957bd09a10','minnesota-river','Minnesota River',ST_GeomFromText('POLYGON ((-112000 2602000, 234000 2602000, 234000 2244000, -112000 2244000, -112000 2602000))',5070),'2cf60156-f22a-418a-bc9f-a28960ad0321'),
     ('c572ed70-d401-4a97-aea6-cb3fe2b77e41','savannah-river-basin','Savannah River Basin',ST_GeomFromText('POLYGON ((1110000 1432000, 1432000 1432000, 1432000 1094000, 1110000 1094000, 1110000 1432000))',5070),'0154184e-2509-4485-b449-8eff4ab52eef');
 
 -- watershed_usgs_sites
