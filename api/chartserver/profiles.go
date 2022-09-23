8004fa3d77ff1df6445c8c72fce2368eccb08e47
Adam Scarberry <46494851+adamscarberry@users.noreply.github.com>
2022-09-23 09:26:07 -0400

    additional inputs

--- api/chartserver/profiles.go
+++ api/chartserver/profiles.go
@@ -5,15 +5,17 @@ import (
 )
 
 type DamProfileChartInput struct {
-	Pool    float64 `querystring:"pool"`
-	Tail    float64 `querystring:"tail"`
-	Inflow  float64 `querystring:"inflow"`
-	Outflow float64 `querystring:"outflow"`
+	Pool      float64 `querystring:"pool"`
+	Tail      float64 `querystring:"tail"`
+	Inflow    float64 `querystring:"inflow"`
+	Outflow   float64 `querystring:"outflow"`
+	DamTop    float64 `querystring:"damTop"`
+	DamBottom float64 `querystring:"damBottom"`
 }
 
 func (s ChartServer) DamProfileChart(input DamProfileChartInput) (string, error) {
 	u := *s.URL
-	u.Path = u.Path + "/example-scatter"                     // Build URL Path
+	u.Path = u.Path + "/dam-profile-chart"                   // Build URL Path
 	u.RawQuery = helpers.StructToQueryValues(input).Encode() // Build URL Query Params
 	return helpers.HTTPGet(&u)
 }
