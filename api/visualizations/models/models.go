7a2876172b7d43ba5645546794881d7047d598ff
Adam Scarberry <46494851+adamscarberry@users.noreply.github.com>
2022-10-18 13:29:55 -0400

    add provider and datasource_type to mapping payload (#109)

--- api/visualizations/models/models.go
+++ api/visualizations/models/models.go
@@ -20,6 +20,7 @@ type VisualizationMapping struct {
 	DatasourceType  string     `json:"datasource_type,omitempty" query:"datasource_type"`
 	LatestTime      *time.Time `json:"latest_time,omitempty"`
 	LatestValue     *float64   `json:"latest_value,omitempty"`
+	Provider        string     `json:"provider,omitempty" db:"provider"`
 }
 
 type Visualization struct {
@@ -71,27 +72,40 @@ func ListVisualizations(db *pgxpool.Pool) ([]Visualization, error) {
 // GetVisualization returns a single Visualization
 func GetVisualization(db *pgxpool.Pool, visualizationSlug *string) (*Visualization, error) {
 
-	var getVisualizationSQL = `SELECT
-							l.slug AS location_slug,
-							v.name AS name,
-							v.slug AS slug,
-							v.type_id,
-							p."name" AS provider_name, 
-							p.slug AS provider_slug,
-							COALESCE(json_agg(json_build_object(
-								'variable', vvm.variable,
-								'key', t.datasource_key,
-								'latest_time', t.latest_time,
-								'latest_value', t.latest_value
-							)), '[]') AS mapping
-							FROM visualization v
-							LEFT JOIN "location" l ON l.id = v.location_id
-							JOIN provider p ON p.id = v.provider_id
-							LEFT JOIN visualization_variable_mapping vvm ON vvm.visualization_id = v.id
-							LEFT JOIN timeseries t ON t.id = vvm.timeseries_id
-							WHERE lower(v.slug) = lower($1)
-							GROUP BY l.slug, v.slug, v.name, v.type_id, p.name, p.slug
-							LIMIT 1`
+	var getVisualizationSQL = `
+								WITH timeseries_providers AS (
+									-- make sure we get the provider attached
+									-- to the timeseries instead of the viz/chart
+									SELECT t.id, p.slug FROM provider p 
+									JOIN datasource d ON d.provider_id = p.id
+									JOIN timeseries t ON t.datasource_id = d.id 
+								)
+								SELECT
+								l.slug AS location_slug,
+								v.name AS name,
+								v.slug AS slug,
+								v.type_id,
+								p."name" AS provider_name, 
+								p.slug AS provider_slug,
+								COALESCE(json_agg(json_build_object(
+									'variable', vvm.variable,
+									'datasource_type', dt.slug,
+									'provider', tp.slug,
+									'key', t.datasource_key,
+									'latest_time', t.latest_time,
+									'latest_value', t.latest_value
+								)), '[]') AS mapping
+								FROM visualization v
+								LEFT JOIN "location" l ON l.id = v.location_id
+								JOIN provider p ON p.id = v.provider_id
+								LEFT JOIN visualization_variable_mapping vvm ON vvm.visualization_id = v.id
+								LEFT JOIN timeseries t ON t.id = vvm.timeseries_id
+								LEFT JOIN datasource d ON d.id = t.datasource_id 
+								LEFT JOIN datasource_type dt ON dt.id = d.datasource_type_id
+								LEFT JOIN timeseries_providers tp ON tp.id = t.id
+								WHERE lower(v.slug) = lower($1)
+								GROUP BY l.slug, v.slug, v.name, v.type_id, p.name, p.slug
+								LIMIT 1`
 
 	var v Visualization
 	//if err := pgxscan.Get(context.Background(), db, &v, listVisualizationsSQL+" WHERE lower(v.slug) = lower($1)", visualizationSlug); err != nil {
@@ -188,16 +202,19 @@ func CreateOrUpdateVisualizationMapping(db *pgxpool.Pool, c VisualizationMapping
 				$2,
 				(SELECT t.id from timeseries t
 					JOIN datasource d ON d.id = t.datasource_id 
-					JOIN datasource_type dt ON dt.id = d.datasource_type_id 
+					JOIN datasource_type dt ON dt.id = d.datasource_type_id
+					JOIN provider p ON p.id = d.provider_id 
 					WHERE lower(datasource_key) = lower($3)
-					AND lower(dt.slug) = lower($4))
+					AND lower(dt.slug) = lower($4)
+					AND lower(p.slug) = lower($5)
+				)
 			)
 			ON CONFLICT ON CONSTRAINT visualization_id_unique_variable
 			DO UPDATE SET
 			variable = $2
 			WHERE visualization_variable_mapping.visualization_id = (SELECT id from visualization WHERE lower(slug) = lower($1))
 			RETURNING visualization_id`,
-			*visualizationSlug, v.Variable, v.Key, v.DatasourceType,
+			*visualizationSlug, v.Variable, v.Key, v.DatasourceType, v.Provider,
 		)
 		if err != nil {
 			return make([]VisualizationMapping, 0), err
