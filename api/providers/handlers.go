0f07613c9554e08d984525744e3f89b1f5abfd15
Jeff Gregory <jeffrey.s.gregory@me.com>
2022-10-28 09:56:37 -0500

    Added datasources endpoint under providers

--- api/providers/handlers.go
+++ api/providers/handlers.go
@@ -8,10 +8,20 @@ import (
 )
 
 func (s Store) ListProviders(c echo.Context) error {
-
 	pp, err := models.ListProviders(s.Connection)
 	if err != nil {
 		return c.String(http.StatusInternalServerError, err.Error())
 	}
 	return c.JSON(http.StatusOK, pp)
+}
+
+// ListDatasources
+func (s Store) ListDatasources(c echo.Context) error {
+	p := c.QueryParam("provider")
+	d := c.QueryParam("datasource_type")
+	dd, err := models.ListDatasources(s.Connection, p, d)
+	if err != nil {
+		return c.String(http.StatusInternalServerError, err.Error())
+	}
+	return c.JSON(http.StatusOK, dd)
 }
