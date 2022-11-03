6a43203e3696739425c46d5e78a1de320709b1d8
Adam Scarberry <46494851+adamscarberry@users.noreply.github.com>
2022-11-03 11:41:57 -0400

    nws levels seed and test

--- sql/local/V999.3.1__NWS_Levels_seed.sql
+++ sql/local/V999.3.1__NWS_Levels_seed.sql
@@ -14,9 +14,9 @@ BEGIN
 
     -- Kanawha Falls at Kanawha River, WV
     INSERT into timeseries(datasource_id, datasource_key, latest_time, latest_value) VALUES
-        (nws_level_ds_id, 'KANW2.Stage.Inst.0.Action', '2021-09-19T18:35:00Z', 25),
-        (nws_level_ds_id, 'KANW2.Stage.Inst.0.Flood', '2021-09-19T18:35:00Z', 27),
-        (nws_level_ds_id, 'KANW2.Stage.Inst.0.Moderate Flood', '2021-09-19T18:35:00Z', 30),
-        (nws_level_ds_id, 'KANW2.Stage.Inst.0.Major Flood', '2021-09-19T18:35:00Z', 35);
+        (nws_level_ds_id, 'KANW2.Stage.Action', '2021-09-19T18:35:00Z', 25),
+        (nws_level_ds_id, 'KANW2.Stage.Flood', '2021-09-19T18:35:00Z', 27),
+        (nws_level_ds_id, 'KANW2.Stage.Moderate Flood', '2021-09-19T18:35:00Z', 30),
+        (nws_level_ds_id, 'KANW2.Stage.Major Flood', '2021-09-19T18:35:00Z', 35);
 
 END$$;
